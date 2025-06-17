59e2154a63a5da561c90d9db73e8afe4b4a89d58
Fernando Martins <fernandom.adm@gmail.com>
2025-06-16 21:32:30 -0300

    Adicionando gitignore

---
+++ apresentacao.ts
@@ -0,0 +1,16 @@
+console.log("Digite o seu nome:");
+let nome : string;
+console.log("Olá, meu nome é "+nome+"!");
+
+console.log("Digite o seu código:");
+let codigo : number;
+console.log("Meu código de aluno é "+codigo);
+
+console.log("Atualmente está estudando no SuperDev de Java Full Stack?:");
+let cursando : boolean = true;
+
+console.log("Digite o mês:");
+let mes : string;
+
+console.log("")
+
