35447d89e745e6a19645e3b6cad4bbca055dddad
Fernando Martins <fernandom.adm@gmail.com>
2025-06-23 21:02:20 -0300

    Adicionando exercicio com Classe Aluno - Calculo da média

--- Aluno.ts
+++ Aluno.ts
@@ -3,16 +3,51 @@ export class Aluno {
     public readonly codigo: number;
     public curso: string= "";
     public matricula: boolean = true;
-    public readonly notas: number[] = [];
+    public readonly notas: number[] = [1,3,5,5];
 
     constructor(nome: string, codigo: number){
         this.nome = nome;
         this.codigo = codigo;
     }
+     verificarMatricula() : void {
+        if(this.matricula === true){ /* O operador === é chamado de "estritamente igual" (strict equality). Ele compara valor e tipo. Ou seja, só retorna true se ambos forem exatamente iguais tanto no conteúdo quanto no tipo de dado. */
+            console.log(`${this.nome} está com matricula ativa`)
+            return;
+        }
+        console.log(`Encontramos problemas na matricula de ${this.nome}`)
+    }
 
+    calcularMedia(){
+        if(this.notas.length < 4){
+            console.log("Lançar 4 notas para poder calcular a média");
+            return;
+        }
+        /* A função reduce() serve para reduzir um array a um único valor, acumulando os valores conforme você define na função. */
+        const soma = this.notas.reduce((acumulador,notas) => acumulador + notas, 0);
+        const media = (soma / this.notas.length);
+
+        console.log(`A média final foi:${media.toFixed(2)}`)
+
+        if(media >= 8){
+            console.log("Aprovado para o próximo período.");
+            return;
+        }    
+        if(media > 4 && media <= 7.9){
+            console.log("Retido em período de exames.");
+            return;
+        }
+        if(media <= 4){
+            console.log("Retenação direta.");
+        }
+    }
+    
 }
-
 
     const aluno1: Aluno = new Aluno('Fernando',2000);
     
     console.log("Nome:"+aluno1.nome+" |Código:"+ aluno1.codigo);
+
+
+    aluno1.verificarMatricula();
+
+    aluno1.calcularMedia();
