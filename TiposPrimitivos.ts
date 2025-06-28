82fd3645a4ef053b8cb399550b9f2d3b923b06a1
Fernando Martins <fernandom.adm@gmail.com>
2025-06-28 20:19:04 -0300

    Tipos Primitivos

---
+++ TiposPrimitivos.ts
@@ -0,0 +1,57 @@
+var num: number = 10;
+var boolean : boolean = true;
+var str : string = "A beautiful message";
+var str2: string = "A beautiful message";
+var str3: string = `${str2} to Fernando`;
+console.log(str3);
+
+function func(): void {
+
+}
+
+if(boolean != null){
+
+}
+if (boolean != undefined){
+}
+
+if (boolean){
+    let i: number;
+    for(i = 0; 1 < 3; i++){
+        console.log(i);
+    }
+}
+/* console.log("Value of"+i) */
+let lista: number[] = [1,2,3]; 
+let lista2: Array<number> = [1,2,3];
+let tuple: [string,number];
+
+tuple = ["Fernando",33];
+
+console.log(tuple[0].toLocaleLowerCase());
+
+enum Day{Segunda,Terça,Quarta,Quinta,Sexta};
+
+const meuNome: string = "Fernando";
+const numero: number = 1;
+const myboolean: boolean = true;
+
+const myNull: null = null; /* Define variavel como nulo */
+
+const myUndefined: undefined = undefined; /* Define variavel como indefinido */
+
+const myName: string = "Maria";
+
+function soma(a: number, b: number): number{
+    return a + b;
+}
+
+function greet(name: string, age?: number){ /* pont de interrogação deixa a variavel opcional. */
+    
+}
+
+greet(myName);
+
+
+
+
